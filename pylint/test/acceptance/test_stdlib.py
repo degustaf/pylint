dc9b94210927234123abea420ceb2ebdd1bdd1b1
Łukasz Rogalski <rogalski.91@gmail.com>
2017-03-04 07:58:24 +0100

    add extra locations to check

--- pylint/test/acceptance/test_stdlib.py
+++ pylint/test/acceptance/test_stdlib.py
@@ -2,20 +2,34 @@
 # For details: https://github.com/PyCQA/pylint/blob/master/COPYING
 
 import os
+import zipfile
 
 import pytest
 
 import pylint.lint
 
-LIB_DIR = os.path.dirname(os.__file__)
-MODULES_TO_CHECK = [f for f in os.listdir(LIB_DIR) if f.endswith(".py")
-                    or os.path.exists(os.path.join(LIB_DIR, f, '__init__.py'))]
-MODULES_TO_CHECK = MODULES_TO_CHECK
+
+def is_module(filename):
+    return filename.endswith(".py")
 
 
-@pytest.mark.parametrize("test_module_name", MODULES_TO_CHECK)
-def test_libmodule(test_module_name):
-    os.chdir(LIB_DIR)
+def is_package(filename, location):
+    return os.path.exists(os.path.join(location, filename, '__init__.py'))
+
+# TODO: is there a deterministic way
+LIB_DIRS = [
+    os.path.dirname(os.__file__),
+    os.path.dirname(zipfile.__file__)
+]
+MODULES_TO_CHECK = [(location, module) for location in LIB_DIRS for module in os.listdir(location)
+                    if is_module(module) or is_package(module, location)]
+MODULES_NAMES = [m[1] for m in MODULES_TO_CHECK]
+
+
+@pytest.mark.parametrize(("test_module_location", "test_module_name"),
+                         MODULES_TO_CHECK, ids=MODULES_NAMES)
+def test_libmodule(test_module_location, test_module_name):
+    os.chdir(test_module_location)
     try:
         pylint.lint.Run([test_module_name])
     except SystemExit as ex:
